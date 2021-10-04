69a785e591ede63628b6f73c131ced1b9c3ae794
Shu Han Tan <shuhan.tan@gmail.com>
2021-10-04 03:21:12 +0000

    first commit

---
+++ quiz.js
@@ -0,0 +1,30 @@
+
+
+//Array o f objects
+const quiz = [
+    {
+        q:'Which actor plays Sherlock Holmes?',
+        options:['Steven Moffat','Tom Holland','Mark Gatiss','Benedict Cumberbatch'],
+        answer:3
+    },
+    {
+        q:'In "The Reichenbach Fall", Moriarty uses a fake name to try and mess with Sherlock. What is it?',
+        options:['Jim Smith','Richard Brook','Steven Moriarty','Derren Brown'],
+        answer:1
+    },
+    {
+        q:'Who sent the text "I\'m not hungry, let\'s have dinner." to Sherlock',
+        options:['Molly Hooper','Jim Moriarty','Irene Adler','John Watson'],
+        answer:2
+    },
+    {
+        q:'What does Jim Moriarty use to smash the glass casing surrounding the crown jewels?',
+        options:['A fire extinguisher','A hammer','A chair','A baseball bat'],
+        answer:0
+    },
+    {
+        q:'What are the first words ever spoken by Sherlock in the show?',
+        options:['"What happened"','"Hello!"','"Can I borrow your phone?"','"How fresh?"'],
+        answer:3
+    }
+    ]
